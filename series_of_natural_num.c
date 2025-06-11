e51acfad60276345dce06f9eb7f1cccbd1f4f249
Mohd Faizan <sahzadfaizan78@gmail.com>
2025-06-12 01:16:00 +0530

    Add files via upload

---
+++ series_of_natural_num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+int  i , n;
+printf("Enter the number n : ");
+scanf("%d",&n);
+printf("The series of n natural number :");
+for ( i = 1 ; i <= n; i++)
+{
+   
+    printf("%d ",i);
+}
+
+    return 0;
+}
