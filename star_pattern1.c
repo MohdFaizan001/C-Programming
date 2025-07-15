dccc7c72f2beb22b17d3cd7eb98bc9ddc104ebc4
Mohd Faizan <sahzadfaizan78@gmail.com>
2025-07-15 15:55:48 +0530

    Add files via upload

---
+++ star_pattern1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/* Displaying pattern: *
+                       **
+                       ***
+                       ****     */
+int main(){
+    int n;  // n is no. of lines
+
+    printf("Enter the number:");
+    scanf("%d",&n);
+
+    for (int i = 0; i < n ; i++)
+    {
+       for (int j = 0; j <= i; j++)
+       {
+        printf("*");
+       }
+       printf("\n");
+       
+    }
+    
+    return 0;
+}
